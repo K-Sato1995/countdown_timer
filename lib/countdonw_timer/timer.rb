49a9cb006540a207e4e224ce22a17941ad92b6ce
Katsuki <Katsuki_Sato0130@icloud.com>
2018-10-30 07:17:31 +0900

    Dryed up a bit

--- lib/countdonw_timer/timer.rb
+++ lib/countdonw_timer/timer.rb
@@ -7,43 +7,23 @@ module CountdonwTimer
         remaining_day = 0
 
         if now > destination_time
-          total_hours = (now - destination_time) / 60 / 60
+          total_secs = now - destination_time
+          total_hours = total_secs / 60 / 60
           remaining_day = total_hours / 24 if total_hours > 24
           remaining_hour = total_hours - (remaining_day * 24)
-          remaining_min = (now - destination_time) / 60 % 60
-          remaining_sec = (now - destination_time) - ((remaining_day * 24 * 60 * 60) + (remaining_hour * 60 * 60) + (remaining_min * 60))
+          remaining_min = total_secs / 60 % 60
+          remaining_sec = total_secs - ((remaining_day * 24 * 60 * 60) + (remaining_hour * 60 * 60) + (remaining_min * 60))
         else
-          total_hours = (destination_time - now) / 60 / 60
+          total_secs = destination_time - now
+          total_hours = total_secs / 60 / 60
           remaining_day = total_hours / 24 if total_hours > 24
           remaining_hour = total_hours - (remaining_day * 24)
-          remaining_min = (destination_time - now) / 60 % 60
-          remaining_sec = (destination_time - now) - ((remaining_day * 24 * 60 * 60) + (remaining_hour * 60 * 60) + (remaining_min * 60))
+          remaining_min = total_secs / 60 % 60
+          remaining_sec = total_secs - ((remaining_day * 24 * 60 * 60) + (remaining_hour * 60 * 60) + (remaining_min * 60))
         end
 
         return "#{remaining_day}/#{remaining_hour}:#{remaining_min}:#{remaining_sec}"
       end
-    end
-
-    def set_timer(year, month = 1, day = 1, hour = 0, min = 0, sec = 0)
-      now = Time.now.to_i
-      destination_time = Time.new(year, month, day, hour, min, sec).to_i
-      remaining_day = 0
-
-      if now > destination_time
-        total_hours = (now - destination_time) / 60 / 60
-        remaining_day = total_hours / 24 if total_hours > 24
-        remaining_hour = total_hours - (remaining_day * 24)
-        remaining_min = (now - destination_time) / 60 % 60
-        remaining_sec = (now - destination_time) - ((remaining_day * 24 * 60 * 60) + (remaining_hour * 60 * 60) + (remaining_min * 60))
-      else
-        total_hours = (destination_time - now) / 60 / 60
-        remaining_day = total_hours / 24 if total_hours > 24
-        remaining_hour = total_hours - (remaining_day * 24)
-        remaining_min = (destination_time - now) / 60 % 60
-        remaining_sec = (destination_time - now) - ((remaining_day * 24 * 60 * 60) + (remaining_hour * 60 * 60) + (remaining_min * 60))
-      end
-
-      return "#{remaining_day}/#{remaining_hour}:#{remaining_min}:#{remaining_sec}"
     end
   end
 end
